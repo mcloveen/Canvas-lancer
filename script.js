d56f566f34358ea1e9cbb4fe94891299d6b95191
mcloveen <129727789+mcloveen@users.noreply.github.com>
2023-06-14 20:11:39 +0400

    second

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
   
 // let toggleNavv = document.querySelector('nav__list')
 
+
 function toggleNav() {
   let nav = document.querySelector('.nav__list');
   nav.classList.toggle('nav__list--active');
 }
+
 
 let burger = document.querySelector('.header__burger');
 burger.addEventListener('click', toggleNav);
