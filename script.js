478a12829362579596c48e077880531b49cd4968
mcloveen <129727789+mcloveen@users.noreply.github.com>
2023-06-14 12:54:25 +0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let toggleNav = document.querySelector('nav__list')
+
 function toggleNav() {
   let nav = document.querySelector('.nav__list');
   nav.classList.toggle('nav__list--active');
